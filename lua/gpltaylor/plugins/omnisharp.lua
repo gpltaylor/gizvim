9c92beddc242a05906b1d82fca860a72df937f64
Garry Taylor <garrytaylor@fluentmoney.co.uk>
2024-06-15 09:54:20 +0100

    OmniSharp - Change up keymappings

--- lua/gpltaylor/plugins/omnisharp.lua
+++ lua/gpltaylor/plugins/omnisharp.lua
@@ -2,8 +2,16 @@ return {
   "OmniSharp/omnisharp-vim",
   config= function()
 
-    vim.keymap.set("n", "<leader>ofu", ":OmniSharpFindUsages<cr>", { desc = "Omnishap  - Find References"})
+    vim.keymap.set("n", "<leader>gu", ":OmniSharpFindUsages<cr>", { desc = "Omnishap  - Find References"})
+    -- I want to do to the implementation of a method when jumping - I know this is backwards but so am I
+    vim.keymap.set("n", "<leader>gd", ":OmniSharpFindImplementation<cr>", { desc = "Omnishap  - Find Implementation"})
+    vim.keymap.set("n", "<leader>gs", ":OmniSharpFindSymbol<cr>", { desc = "Omnishap  - Find Symbol"})
+
+    vim.keymap.set("n", "<leader>od", ":OmniSharpDocumentation<cr>", { desc = "Omnishap  - Documentation"})
+    vim.keymap.set("n", "<leader>ofi", ":OmniSharpFindImplementation<cr>", { desc = "Omnishap  - Find Implementation"})
     vim.keymap.set("n", "<leader>ogd", ":OmniSharpGotoDefinition<cr>", { desc = "Omnishap  - Find definition"})
+    vim.keymap.set("n", "<leader>ofs", ":OmniSharpFindSymbol<cr>", { desc = "Omnishap  - Find Symbol"})
+    vim.keymap.set("n", "<leader>ofu", ":OmniSharpFindUsages<cr>", { desc = "Omnishap  - Find References"})
 
     --OmniSharpGotoDefinition 
   
