33e687965d5945d8abae0b3f8ec519a332e11477
Garry Taylor <garry.taylor@redbear.co.uk>
2025-04-18 10:36:27 +0100

    Include Vue plugin

---
+++ lua/gpltaylor/plugins/vim-vue.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "posva/vim-vue",
+    event = "VeryLazy",
+  },
+  {
+    "neoclide/coc.nvim",
+  },
+  {
+    "yaegassy/coc-volar",
+    build = "yarn install --frozen-lockfile",
+  },
+  {
+    "yaegassy/coc-volar-tools",
+    build = "yarn install --frozen-lockfile",
+  },
+}
