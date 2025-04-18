c7e79b9e0c6523660682926475b78bb3fba9b006
Garry Taylor <garry.taylor@redbear.co.uk>
2025-04-18 11:15:27 +0100

    Tree FindFile - Bugfix
    Signed-off-by: Garry Taylor <garry.taylor@redbear.co.uk>

--- lua/gpltaylor/plugins/nvim-tree.lua
+++ lua/gpltaylor/plugins/nvim-tree.lua
@@ -49,7 +49,7 @@ return {
     local keymap = vim.keymap                                                                                            -- for conciseness
 
     keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })                          -- toggle file explorer
-    keymap.set("n", "<leader>tff", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>tff", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>tc", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                      -- collapse file explorer
     keymap.set("n", "<leader>tr", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })                        -- refresh file explorer
     keymap.set("n", "<leader>tf", "<cmd>NvimTreeFocus<CR>", { desc = "Focus on Tree" })                                  -- refresh file explorer
